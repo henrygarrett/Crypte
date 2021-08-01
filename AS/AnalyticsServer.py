cb1983ccdd96d3553f8ae5e576f824d23de99ede
Samuel Maddock <samuel-maddock@hotmail.com>
2021-08-01 15:51:35 +0100

    Added init_executor() to AnalyticsServer

--- AS/AnalyticsServer.py
+++ AS/AnalyticsServer.py
@@ -5,3 +5,10 @@ class AnalyticsServer():
     def __init__(self):
         self.aggregator = Aggregator()
         self.program_executor = ProgramExecutor()
+        self.init_executor()
+
+    def init_executor(self):
+        self.program_executor = ProgramExecutor(self.aggregator.data_encrypted)
+
+    def __str__(self):
+        return "AnalyticsServer" + str({"Aggregator": self.aggregator.__str__(), "ProgramExecutor": self.program_executor.__str__()})
