cf628706e858f1c52c1625861bd5eeb9d61ad957
henrygarrett <52292612+henrygarrett@users.noreply.github.com>
2021-08-12 10:02:12 +0100

    Finishing off budget handling for laplace operator

--- CSP/PrivacyEngine.py
+++ CSP/PrivacyEngine.py
@@ -1,5 +1,5 @@
 import os
-
+import pandas as pd
 class PrivacyEngine():
     def __init__(self, epsilon_budget):
         self.epsilon_budget = epsilon_budget
@@ -12,10 +12,18 @@ class PrivacyEngine():
         if self.epsilon_budget - program_epsilon < 0:
             return False
         else:
+            self.add_to_ledger(program_epsilon)
+            self.epsilon_budget -= program_epsilon
             return True
+
     def add_to_ledger(self, program_epsilon):
-        pass
+        length = pd.read_csv('.' + os.sep + 'budget.txt').shape[0]
+        df = pd.DataFrame({'change': -program_epsilon, 'total': self.epsilon_budget - program_epsilon}, index=[length + 1])
+        df.to_csv('.' + os.sep + 'budget.txt', mode='a',index=False, header=not '.' + os.sep + 'budget.txt')
 
     def save_epsilon_budget(self):
-        with open('.' + os.sep + 'budget.txt', 'w') as budget_file:
-            budget_file.write(str(self.epsilon_budget))
+        df = pd.DataFrame({'change': self.epsilon_budget, 'total': self.epsilon_budget}, index=[0])
+        df.to_csv('.' + os.sep + 'budget.txt',index=False)
+
+
+
